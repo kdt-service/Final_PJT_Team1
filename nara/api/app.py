3a83b4894fcb796bb688abd8ad388f4bd569c93c
nara132 <awdee14@naver.com>
2023-06-20 14:56:43 +0900

    commit by nara update api.py

--- nara/api/app.py
+++ nara/api/app.py
@@ -25,13 +25,42 @@
 from flask import Flask, request, jsonify
 from PIL import Image
 import numpy as np
+from flask_mysqldb import MySQL
+import pandas as pd
 
+
+DBconfig = {}
+with open('config/DBconfig', 'r') as f:
+    for l in f.readlines():
+        k, v = l.rstrip().split('=')
+        DBconfig[k] = v
 # 앱 생성
 app = Flask(__name__)
 
+# DB 연동
+app.config['MYSQL_HOST'] = DBconfig['host']
+app.config['MYSQL_PORT'] = int(DBconfig['port'])  
+app.config['MYSQL_USER'] = DBconfig['user']
+app.config['MYSQL_PASSWORD'] = DBconfig['passwd']
+app.config['MYSQL_DB'] = DBconfig['db']
+
+mysql = MySQL(app)
+
+# 이상치 제거함수
+def del_outlier(price : pd.Series) :
+    des = price.describe()
+    q1 = des['25%']
+    q3 = des['75%']
+    iqr = q3-q1
+    lower_bound = q1 - 1.5 * iqr
+    upper_bound = q3 + 1.5 * iqr
+    
+    result = price[(price >= lower_bound) & (price <= upper_bound) & (price >= 5000)]
+    
+    return result
+
 # 모델링 적용 함수
 def model_def(image, *text_data):
-
     # 이미지 전처리
     image = Image.open(image)
     image = image.resize((224, 224))  # 크기 조정 (예시)
@@ -39,11 +68,33 @@ def model_def(image, *text_data):
     image = image / 255.0  # 정규화 (예시)
     image = None # 오류방지코드 (임시)
 
-    return [data for data in text_data], image
+    return [data for data in text_data], image # 결과 (예시)
 
-# 데코레이터로 /nara 경로 생성(POST)
-@app.route('/nara', methods=['POST'])
-def process_data():
+@app.route('/price', methods=['POST'])
+def market_price():
+    brand = request.form['product_brand'] # 나이키, 구찌, 프라다
+    product_type = request.form['product_type'] # 신발, 지갑, 티셔츠
+    cursor = mysql.connection.cursor()
+    cursor.execute('SELECT id, price FROM category_db.bunjang_prd WHERE name LIKE "%{}%" AND name LIKE "%{}%"'.format(brand, product_type))
+    result = cursor.fetchall()
+    cursor.close()
+
+    df_column = ['id','price']
+    df = pd.DataFrame(result, columns=df_column)
+    df['price'] = del_outlier(df['price'])
+
+    mean_price = df['price'].mean()
+    r_price = int(round(mean_price, -len(str(int(mean_price))) + 3))
+
+    response = {
+        'market_price': r_price
+    }
+
+    return jsonify(response)
+
+# 데코레이터로 /cate 경로 생성(POST)
+@app.route('/cate', methods=['POST'])
+def cate():
     
     # 데이터 key:value 확인
     if 'product_name' not in request.form or 'product_info' not in request.form or 'product_image' not in request.files:
@@ -67,4 +118,5 @@ def process_data():
     return jsonify(response) # json 객체로 반환
 
 if __name__ == '__main__':
+    # app.run(host='0.0.0.0', port=8080)
     app.run(debug=True)
