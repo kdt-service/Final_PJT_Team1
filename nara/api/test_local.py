5c25767bb96abcaa9c3e133e224d6fb2089a1be3
nara132 <awdee14@naver.com>
2023-06-20 15:02:23 +0900

    commit by nara

--- nara/api/test_local.py
+++ nara/api/test_local.py
@@ -2,21 +2,31 @@ import requests
 import json
 
 
-image_path = 'image.jpg'
+# image_path = 'image.jpg'
 
-# 상품명과 상품정보
-product_name = '상품명'
-product_info = '상품 정보'
+# # 상품명과 상품정보
+# product_name = '상품명'
+# product_info = '상품 정보'
 
-# API 엔드포인트 URL
-url = 'http://localhost:5000/nara'
+# # API 엔드포인트 URL
+# url = 'http://localhost:5000/cate'
 
-# 이미지 파일과 상품명, 상품정보
-files = {'product_image': open(image_path, 'rb')}
-data = {'product_name': product_name, 'product_info': product_info}
-response = requests.post(url, files=files, data=data)
+# # 이미지 파일과 상품명, 상품정보
+# files = {'product_image': open(image_path, 'rb')}
+# data = {'product_name': product_name, 'product_info': product_info}
+# response = requests.post(url, files=files, data=data)
+
+# result = response.json()
+
+# print(result)
+
+
+url2 = 'http://localhost:5000/price'
+brand = '구찌'
+typ = '신발'
+data = {'product_brand': brand, 'product_type': typ}
+response = requests.post(url2, data=data)
 
 result = response.json()
 
 print(result)
-
